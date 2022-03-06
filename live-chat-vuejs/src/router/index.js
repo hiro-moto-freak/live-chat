7958f5a5861ab572f24e1cfeeffaa11cf3bb9a24
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-03-06 23:55:21 +0900

    ログイン状態によるアクセス制限機能の実装

--- live-chat-vuejs/src/router/index.js
+++ live-chat-vuejs/src/router/index.js
@@ -1,18 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/WelcomePage'
 import ChatRoom from '../views/ChatRoom'
+import useValidate from '../auth/validate'
+
+const { error, validate } = useValidate()
+
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしてません')
+    next({ name: 'WelcomePage' })
+    return
+  }
+
+  await validate()
+
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'WelcomePage' })
+  } else {
+    next()
+  }
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'ChatRoom' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'WelcomePage',
-    component: Welcome
+    component: Welcome,
+    beforeEnter: noRequireAuth
   },
 
   {
     path: '/chatroom',
     name: 'ChatRoom',
-    component: ChatRoom
+    component: ChatRoom,
+    beforeEnter: requireAuth
   }
 ]
 
