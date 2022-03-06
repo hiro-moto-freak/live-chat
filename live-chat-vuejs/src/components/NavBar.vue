1e721dd5f354a7b26b9709e2207bb8d7b6cf974a
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-03-06 17:08:35 +0900

    ログアウト機能の実装

--- live-chat-vuejs/src/components/NavBar.vue
+++ live-chat-vuejs/src/components/NavBar.vue
@@ -5,22 +5,55 @@
         こんにちは、<span class="name">{{ name }}</span
         >さん
       </p>
-      <p class="email">
-        <span class="email">{{ email }}</span
-        >でログイン中です
-      </p>
+      <p class="email">現在、{{ email }}でログイン中です</p>
+      <div class="error">{{ error }}</div>
     </div>
-    <button>ログアウト</button>
+    <button @click="logout">ログアウト</button>
   </nav>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       name: window.localStorage.getItem("name"),
       email: window.localStorage.getItem("uid"),
+      error: null,
     };
+  },
+  methods: {
+    async logout() {
+      this.error = null;
+
+      try {
+        const res = await axios.delete("http://localhost:3000/auth/sign_out", {
+          headers: {
+            uid: this.email,
+            "access-token": window.localStorage.getItem("access-token"),
+            client: window.localStorage.getItem("client"),
+          },
+        });
+
+        if (!res) {
+          new Error("ログアウトできませんでした");
+        }
+
+        if (!this.error) {
+          console.log("ログアウトしました");
+          window.localStorage.removeItem("access-token");
+          window.localStorage.removeItem("client");
+          window.localStorage.removeItem("uid");
+          window.localStorage.removeItem("name");
+
+          this.$router.push({ name: "WelcomePage" });
+        }
+
+        return res;
+      } catch (error) {
+        this.error = "ログアウトできませんでした";
+      }
+    },
   },
 };
 </script>
