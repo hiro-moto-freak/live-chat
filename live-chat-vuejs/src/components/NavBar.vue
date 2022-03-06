f3605b4794b8066cbfac212590054599943f463c
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-03-06 13:49:12 +0900

    ナビバーの作成

---
+++ live-chat-vuejs/src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div>
+      <p>こんにちは、XXさん</p>
+      <p class="email">現在..@..でログイン中です</p>
+    </div>
+    <button>ログアウト</button>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email {
+  font-size: 14px;
+  color: #999;
+}
+</style>
